047b11aae09b0e9cd074ee279c2a75d92794e881
Matheus Barroso <ma.albarroso@gmail.com>
2023-10-14 00:22:24 -0300

    Adição de comentários e pequenas mudanças

--- GamingMode.py
+++ GamingMode.py
@@ -2,33 +2,41 @@ import subprocess
 import time
 import pyautogui as pygui
 
+# Caminho padrão para os atalhos
 path = "C:\\Arquivos de Desenvolvimento\\Learning_Python\\GamingMode_Script\\Atalhos"
 
+# Atalhos dos programas no diretório
 xbox = "\\XboxPcApp.exe.lnk"
 battlenet = "\\Battle.net Launcher.exe.lnk"
-steam = "\\Steam.exe.lnk"
 epic = "\\EpicGamesLauncher.exe.lnk"
 discord = "\\Discord.exe.lnk"
+steam = "\\Steam.exe.lnk"
 
-Apps = [xbox, battlenet, epic, discord]
+# Array para organizar ordem de execução dos programas
+Apps = [xbox, battlenet, epic, discord, steam]
 
 print("~~~ Inicializando Programas... ~~~")
 
+# Repetição para execução dos programas
 for app in Apps:
     try:
-        open = (path + app)
+        opening = path + app
         print(f"\nAbrindo '{app}'!")
-        subprocess.run(open, shell=True)
+        subprocess.run(opening, shell=True)
     except FileNotFoundError:
-        print(f"\nO arquivo '{open}' não foi encontrado.")
+        print(f"\nO arquivo '{opening}' não foi encontrado.")
     except Exception as e:
         print(f"\nOcorreu um erro ao tentar executar o arquivo: {e}")
     else:
         print(f"\n '{app}' aberto com sucesso!")
-        #pygui.hotkey('alt', 'f4')
+        # pygui.hotkey('alt', 'f4')
 
-    if(app != Apps[-1]):
+    # Condicional para não mostrar a mensagem de próximo na execução do último programa
+    if app != Apps[-1]:
         print("\nAguardando para abrir o próximo...")
+        # Rotina de delay para não sobrecarregar o computador com muitos processos
         time.sleep(8)
+
+# Add alguma rotina para fechar o programa caso ele pare em um processo e um tempo determinado extrapole
 
 input("\nRotina Finalizada!! :D \nPressione Enter para sair...")
--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+#Teste de formatação visual do texto no terminal
 print("Teste em \033[4msublinhado!\033[0m")
 
 input("Enter...")
